05d6815b97467a41b04813f323c76a6b21555f9d
AnuVedh <anu.hari80@gmail.com>
2026-01-08 14:42:39 -0500

    Updated Basetest

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,7 +1,10 @@
 package baseTest;
 
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import DriverFactory.DriverFactory;
 import pom.PageObjectManager;
@@ -10,13 +13,13 @@ public class BaseTest {
 
 	protected PageObjectManager pom;
 
-	@BeforeClass
+	@BeforeMethod
 	public void launchBrowser() {
 		DriverFactory.initDriver();
 		pom = new PageObjectManager(DriverFactory.getDriver());
 	}
 
-	@AfterClass
+	@AfterMethod
 	public void quitBrowser() {
 
 		if (DriverFactory.getDriver() != null) {
