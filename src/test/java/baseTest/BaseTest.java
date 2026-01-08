4dda0dfa2049de3524bb14ad010bded81ebd3560
AnuVedh <anu.hari80@gmail.com>
2026-01-08 16:26:35 -0500

    pulled Basetest file

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,7 +1,9 @@
 package baseTest;
 
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import driverFactory.DriverFactory;
 import pom.PageObjectManager;
@@ -9,14 +11,26 @@ import pom.PageObjectManager;
 public class BaseTest {
 
 	protected PageObjectManager pom;
+	// private ModuleFlow moduleFlow;
 
-	@BeforeClass
-	public void launchBrowser() {
-		DriverFactory.initDriver();
-		pom = new PageObjectManager(DriverFactory.getDriver());
+	@BeforeSuite
+
+	public void LoadTestdata() throws Exception {
+		Utils.ExcelUtil.getAllsheetData();
+		System.out.println("Loada test data");
 	}
 
-	@AfterClass
+	@BeforeMethod
+	@Parameters({"browser"})
+	public void launchBrowser(String browser) throws Exception {
+		DriverFactory.initDriver(browser);
+		pom = new PageObjectManager(DriverFactory.getDriver());
+		pom.getHomePage().launchApplication();
+		pom.getHomePage().clickGetStarted();
+
+	}
+
+	@AfterMethod
 	public void quitBrowser() {
 
 		if (DriverFactory.getDriver() != null) {
