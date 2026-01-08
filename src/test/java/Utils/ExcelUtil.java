b124674780573b6aaf53783b75cc5695d18949c3
AnuVedh <anu.hari80@gmail.com>
2026-01-08 16:22:42 -0500

    pulled updated ExcelUtil

--- src/test/java/Utils/ExcelUtil.java
+++ src/test/java/Utils/ExcelUtil.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -21,63 +23,7 @@ public class ExcelUtil {
 	private static XSSFWorkbook workbook;
 	private static XSSFSheet sheet;
 
-	@DataProvider(name = "ExcelData")
-	public synchronized Object[][] getData(Method methodName) throws Exception {
-		FileInputStream filelocation = new FileInputStream(filepath);
-		workbook = new XSSFWorkbook(filelocation);
-		// System.out.println(methodName.getName());
-		// System.out.println(methodName.getDeclaringClass());
-		// System.out.println(methodName.getDeclaringClass().getName());
-		String testsheet = getSheetName(
-				methodName.getDeclaringClass().getName());
-		sheet = workbook.getSheet(testsheet);
-		int rows = sheet.getPhysicalNumberOfRows();
-		int cols = sheet.getRow(0).getLastCellNum();
-
-		List<Map<String, String>> data = new ArrayList<>();
-
-		DataFormatter formatter = new DataFormatter();
-
-		for (int i = 1; i < rows; i++) {
-			Map<String, String> testData = new HashMap<>();
-			XSSFRow row = sheet.getRow(i);
-
-			for (int j = 0; j < cols; j++) {
-				String key = sheet.getRow(0).getCell(j).getStringCellValue();
-				String value = (row.getCell(j) == null)
-						? ""
-						: formatter.formatCellValue(row.getCell(j));
-				// : row.getCell(j).toString();
-				testData.put(key, value);
-			}
-			data.add(testData);
-		}
-
-		workbook.close();
-
-		// Convert List<Map> → Object[][]
-		Object[][] finalTestData = new Object[data.size()][1];
-		for (int i = 0; i < data.size(); i++) {
-			finalTestData[i][0] = data.get(i);
-		}
-
-		return finalTestData;
-	}
-
-	private static String getSheetName(String sheetMethod) {
-
-		switch (sheetMethod) {
-			case "TestCase.TestCase_SignIn" :
-				return "Sign-IN";
-			case "TestCase.TestCase_Queue" :
-				return "Queue";
-			case "TestCase.TestCase_Graph" :
-				return "Graph";
-			default :
-				return null;
-		}
-
-	}
+	private static final Map<String, Object[][]> excelData = new HashMap<>();
 
 	public static synchronized List<Map<String, String>> getTestData(
 			String sheetName) throws IOException {
@@ -109,6 +55,83 @@ public class ExcelUtil {
 
 		workbook.close();
 		return testData;
+
+	}
+
+	public static synchronized void getAllsheetData() throws Exception {
+		FileInputStream filelocation = new FileInputStream(filepath);
+		workbook = new XSSFWorkbook(filelocation);
+		for (Sheet sheet : workbook) {
+
+			int rows = sheet.getPhysicalNumberOfRows();
+			int cols = sheet.getRow(0).getLastCellNum();
+			Object[][] data = new Object[rows - 1][1];
+
+			DataFormatter formatter = new DataFormatter();
+
+			for (int i = 1; i < rows; i++) {
+				Map<String, String> testData = new HashMap<>();
+				Row row = sheet.getRow(i);
+
+				for (int j = 0; j < cols; j++) {
+					String key = sheet.getRow(0).getCell(j)
+							.getStringCellValue();
+					String value = (row.getCell(j) == null)
+							? ""
+							: formatter.formatCellValue(row.getCell(j));
+					// : row.getCell(j).toString();
+					testData.put(key, value);
+				}
+
+				data[i - 1][0] = testData;
+
+			}
+			excelData.put(sheet.getSheetName(), data);
+
+		}
+		workbook.close();
+
+	}
+
+	@DataProvider(name = "ExcelData")
+	public static Object[][] getSheetData(Method methodName) {
+		// return excelData.get(sheetName);
+
+		String testsheet = methodName.getDeclaringClass().getName();
+		System.out.println(testsheet);
+
+		switch (testsheet) {
+			case "TestCase.TestCase_SignIn" :
+				// return "Sign-IN";
+				return excelData.get("Sign-IN");
+			case "TestCase.TestCase_Queue" :
+				return excelData.get("Queue");
+			case "TestCase.TestCase_Graph" :
+				return excelData.get("Graph");
+			default :
+				return null;
+		}
+
+	}
+
+	@DataProvider(name = "ExcelDataanu")
+	public static Object[][] getSheetDataforTest(Method methodName) {
+		// return excelData.get(sheetName);
+
+		String testsheet = methodName.getName();
+		System.out.println(testsheet);
+
+		switch (testsheet) {
+			case "TestCase.TestCase_SignIn" :
+				// return "Sign-IN";
+				return excelData.get("Sign-IN");
+			case "TestCase.TestCase_Queue" :
+				return excelData.get("Queue");
+			case "TestCase.TestCase_Graph" :
+				return excelData.get("Graph");
+			default :
+				return null;
+		}
 
 	}
 
