936f5ad2c06a9013221201933d10d97a2a4326da
AnuVedh <anu.hari80@gmail.com>
2026-01-16 16:30:27 -0500

    Updated BaseTest for ChainTestReport

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -1,18 +1,25 @@
 package baseTest;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import com.aventstack.chaintest.plugins.ChainTestListener;
+
 import driverFactory.DriverFactory;
 import pom.PageObjectManager;
-
+@Listeners(ChainTestListener.class)
 public class BaseTest {
 
 	protected PageObjectManager pom;
-
+	 protected WebDriver driver;
 	@BeforeSuite
 
 	public void LoadTestdata() throws Exception {
@@ -30,6 +37,18 @@ public class BaseTest {
 		pom.getHomePage().clickGetStarted();
 
 	}
+	
+	 @AfterMethod()
+	    public void attachScreenShotOnFailure(ITestResult testResult){
+	        if(!testResult.isSuccess()){
+	            ChainTestListener.embed(((TakesScreenshot)(driver)).getScreenshotAs(OutputType.BYTES),"image/png");
+	        }
+	    }
+//	  /*Get Screen Shot*/
+//    public byte[] takeScreenShot(){
+//        return ((TakesScreenshot)(driver)).getScreenshotAs(OutputType.BYTES);
+//    }
+
 
 	@AfterMethod(alwaysRun = true)
 	public void quitBrowser() {
